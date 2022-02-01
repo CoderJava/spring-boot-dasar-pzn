edf683362a86c361764a67fbe6db9222a2ca088d
CoderJava <kolonel.yudisetiawan@gmail.com>
2022-02-01 19:18:31 +0700

    Belajar lifecycle spring

---
+++ src/test/java/programmerzamannow/spring/core/LifeCycleTest.java
@@ -0,0 +1,29 @@
+package programmerzamannow.spring.core;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import programmerzamannow.spring.core.data.Connection;
+
+class LifeCycleTest {
+  private ConfigurableApplicationContext applicationContext;
+
+  @BeforeEach
+  void setUp() {
+    applicationContext = new AnnotationConfigApplicationContext(LifeCycleConfiguration.class);
+    applicationContext.registerShutdownHook(); // otomatis shutdown spring
+  }
+
+  @AfterEach
+  void tearDown() {
+    // applicationContext.close(); manual shutdown spring
+  }
+
+  @Test
+  void testConnection() {
+    Connection connection = applicationContext.getBean(Connection.class);
+  }
+}
